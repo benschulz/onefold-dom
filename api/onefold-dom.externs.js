94346d6c26c13c3183642ee2866757ef0165f158
benshu <benshu@benshu.de>
2015-03-25 15:24:55 +0100

    Added API.

---
+++ api/onefold-dom.externs.js
@@ -0,0 +1,47 @@
+/**
+ * @namespace
+ * @suppress {duplicate}
+ */
+var onefold = onefold || {};
+
+/** @namespace */
+onefold.dom = {};
+
+/**
+ * @template E
+ * @param {Node} root
+ * @param {Node} node
+ * @returns {number}
+ */
+onefold.dom.determineDepth = function (root, node) {};
+
+/**
+ * @param {Node} container
+ * @param {Node} node
+ * @returns {boolean}
+ */
+onefold.dom.isOrContains = function (container, node) {};
+
+/**
+ * @param {Node} container
+ * @param {Node} node
+ * @returns {boolean}
+ */
+onefold.dom.strictlyContains = function (container, node) {};
+
+/** @namespace */
+onefold.dom.element = {};
+
+/**
+ * @param {Element} element
+ * @param {string} selector
+ * @returns {?Element}
+ */
+onefold.dom.element.closest = function (element, selector) {};
+
+/**
+ * @param {Element} element
+ * @param {string} selector
+ * @returns {boolean}
+ */
+onefold.dom.element.matches = function (element, selector) {};
