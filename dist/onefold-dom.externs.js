521dd71aed7261b167f8bf3824b046bb9cb398e4
benshu <benshu@benshu.de>
2015-03-30 21:14:20 +0200

    v0.3.0

---
+++ dist/onefold-dom.externs.js
@@ -0,0 +1,43 @@
+
+
+/** @namespace */
+de.benshu.onefold.dom = {};
+
+/**
+ * @template E
+ * @param {Node} root
+ * @param {Node} node
+ * @returns {number}
+ */
+de.benshu.onefold.dom.determineDepth = function (root, node) {};
+
+/**
+ * @param {Node} container
+ * @param {Node} node
+ * @returns {boolean}
+ */
+de.benshu.onefold.dom.isOrContains = function (container, node) {};
+
+/**
+ * @param {Node} container
+ * @param {Node} node
+ * @returns {boolean}
+ */
+de.benshu.onefold.dom.strictlyContains = function (container, node) {};
+
+/** @namespace */
+de.benshu.onefold.dom.element = {};
+
+/**
+ * @param {Element} element
+ * @param {string} selector
+ * @returns {?Element}
+ */
+de.benshu.onefold.dom.element.closest = function (element, selector) {};
+
+/**
+ * @param {Element} element
+ * @param {string} selector
+ * @returns {boolean}
+ */
+de.benshu.onefold.dom.element.matches = function (element, selector) {};
